4bb7c745288cba65cee07c9e77ffb8cefd37866a
DEdouard22 <dedouard@live.com>
2018-07-26 19:56:35 -0400

    Commit current progress to merge from master

---
+++ client/src/Calendar.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import dateFns from "date-fns"; // will be used to get current date
+import { Container, Row, Col} from 'reactstrap';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
+class Calendar extends Component {
+    constructor(){
+        super();
+        this.state = {
+            currentMonth: new Date(),
+            selectedDate: new Date()
+        }
+    }
+
+    renderHeader() {
+        const dateFormat = "MMMM YYYY";
+    
+        return (
+        <Container>    
+            <Row className="header row flex-middle">    
+                <Col xs="2">    
+                    <PaginationLink previous href="#" onClick={this.prevMonth}/>
+                </Col>
+                <Col xs="8">
+                    <span>{dateFns.format(this.state.currentMonth, dateFormat)}</span>
+                </Col>
+                <Col xs="2">
+                    <PaginationLink next href="#" onClick={this.nextMonth}/>
+                </Col>    
+            </Row>
+        </Container>    
+        );
+    }
+    
+    renderDays() {
+        const dateFormat = "dddd";
+        const days = [];
+    
+        let startDate = dateFns.startOfWeek(this.state.currentMonth);
+    
+        for (let i = 0; i < 7; i++) {
+          days.push(
+            <div className="col col-center" key={i}>
+              {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+            </div>
+          );
+        }
+    
+        return <div className="days row">{days}</div>;
+      }
+
+    onDateClick = day => {
+        this.setState({
+          selectedDate: day
+        });
+      };
+    
+    nextMonth = () => {
+    this.setState({
+        currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+    });
+    };
+    
+    prevMonth = () => {
+    this.setState({
+        currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+    });
+    };
+
+    render() {
+        
+        return (
+            <div className="calendar">
+                {this.renderHeader()}
+                {this.renderDays()}
+            </div>
+        )
+    }
+}
+
+export default Calendar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Calendar from './Calendar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Calendar />, document.getElementById('root'));
 registerServiceWorker();
