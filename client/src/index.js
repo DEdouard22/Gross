30d60f10300d0686ee2ff097b7a0fd9a4a81591b
DEdouard22 <dedouard@live.com>
2018-07-26 20:06:24 -0400

    fixed merged conflicts from Carly's pull request

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-<<<<<<< HEAD
-import Calendar from './Calendar';
-
-ReactDOM.render(<Calendar />, document.getElementById('root'));
-=======
 import { BrowserRouter } from 'react-router-dom';
-
+import Calendar from './Calendar';
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
->>>>>>> master
 registerServiceWorker();
