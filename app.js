a0c70101b8b3e76bdef9b1b18acddebc60c40bc3
Kamilah Amir-Al-Sharif <kamilahamir-al-sharif@Kamilahs-MacBook-Pro-2.local>
2018-08-03 19:49:09 -0400

    kill me now

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var setupAuth = require('./auth');
 
 //auth Kamilah
 var bodyParser = require('body-parser');
@@ -32,6 +33,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+setupAuth(app);
 
 app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
