e2eb8d5ba2c151d2d9070c11e3539ed6878c7200
Gabriel Edouard <edgabriel92@gmail.com>
2018-08-03 22:41:20 -0400

    pulling data from the user table

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var transactionsRouter = require('./routes/transactions');
 
 var app = express();
 
+app.all('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
+  
+  next();
+
+})
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
--- client/src/UserAccount.js
+++ client/src/UserAccount.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Jumbotron, Container, Dropdown, DropdownMenu, DropdownToggle, Progress, Row, Col } from 'reactstrap';
+import { Jumbotron, Container, Dropdown, DropdownMenu, DropdownToggle, Progress, Row, Col, Collapse } from 'reactstrap';
 import './UserAccount.css';
-import User from './User.js';
+import logo from './userIcon.png';
+import axios from 'axios';
 
 class UserAccount extends Component {
     constructor(props) {
@@ -12,29 +13,11 @@ class UserAccount extends Component {
         this.state = {
             dropdownOpenHouse: false,
             dropdownOpenPay: false,
-            users: [
-                {
-                id: 1,
-                name: "User 1",
-                description:"description 1"
-            },
-            {
-                id: 2,
-                name: "User 2",
-                description: "description 2"
-            },
-            {
-                id: 3,
-                name: "User 3",
-                description: "description 3"
-            },
-            {
-                id: 4,
-                name: "User 4",
-                description: "description 4"
-            }
-            ]
+            collapse: false
         };
+
+        this.getUserData = this.getUserData.bind(this);
+        this.imageClick = this.imageClick.bind(this);
     }
 
     togglePayment() {
@@ -49,56 +32,71 @@ class UserAccount extends Component {
         });
     }
 
-    render() {
-        let usersInfo = this.state.users.map(user => {
-            return (
-                <Col key={user.id}>   
-                    <User user={user}/> 
-                </Col> 
-            )
-        })
+    imageClick() {
+        this.setState({ 
+            collapse: !this.state.collapse 
+        });
+    }
 
+    getUserData(){
+        const url = 'http://localhost:3001/api/users/';
+
+        axios.get(`${url}1`)
+        .then(res => {
+            this.setState({
+                firstName: res.data.firstName,
+                lastName: res.data.lastName,
+                email: res.data.email,
+                description: res.data.description
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.getUserData()
+    }
+
+    render() {
         return(
             <div>
                 <Jumbotron className="jumbotron" fluid>
                     <Container fluid>
-                        <h1 className="display-3">Household Name</h1>
-                        <Dropdown isOpen={this.state.dropdownOpenHouse} toggle={this.toggleHousehold}>
-                        <DropdownToggle caret
-                            tag="span" 
-                            onClick={this.toggle} 
-                            data-toggle="dropdown" 
-                            aria-expanded={this.state.dropdownOpenHouse}>    
-                            Household Members
-                        </DropdownToggle>
-                            <DropdownMenu>
-                                <div onClick={this.toggleHousehold}>Household User 1</div>
-                                <div onClick={this.toggleHousehold}>Household User 2</div>
-                            </DropdownMenu>
-                        </Dropdown>
+                        <h2>Welcome</h2>
+                        <h1 className="display-3">{this.state.lastName} Household</h1>
+                        <p className="nav-help">Click icon to display your information</p>
                         <Row>
-                            {usersInfo}
+                            <img onClick={this.imageClick} src={logo} className="userIcon"/>     
                         </Row>
+                        <h2>{this.state.firstName}</h2>
                     </Container>
                 </Jumbotron>
-                <Dropdown isOpen={this.state.dropdownOpenPay} toggle={this.togglePayment}>
-                <div className="payment">
-                    <DropdownToggle caret
-                        tag="span" 
-                        onClick={this.togglePayment} 
-                        data-toggle="dropdown" 
-                        aria-expanded={this.state.dropdownOpenPay}>    
-                        Payment Methods
-                    </DropdownToggle>
-                </div>
-                    <DropdownMenu>
-                        <div onClick={this.togglePayment}>Card option 1</div>
-                        <div onClick={this.togglePayment}>Card option 2</div>
-                    </DropdownMenu>
-                </Dropdown>    
-                    <div className="goals text-left">Saving Goals
-                        <Progress animated color="success" value="25"/>
+                <Collapse isOpen={this.state.collapse}>
+                    <div>
+                        <Dropdown isOpen={this.state.dropdownOpenPay} toggle={this.togglePayment}>
+                            <div className="payment">
+                                <DropdownToggle caret
+                                    tag="span" 
+                                    onClick={this.togglePayment} 
+                                    data-toggle="dropdown" 
+                                    aria-expanded={this.state.dropdownOpenPay}>    
+                                    Payment Methods
+                                </DropdownToggle>
+                            </div>
+                            <div className="expenseDescription">
+                                <ul>
+                                    <li>{this.state.description}</li>
+                                </ul>
+                            </div>
+                            <DropdownMenu>
+                                <div onClick={this.togglePayment}>Card option 1</div>
+                                <div onClick={this.togglePayment}>Card option 2</div>
+                            </DropdownMenu>
+                        </Dropdown>    
+                        <div className="goals text-left">Saving Goals
+                            <Progress animated color="success" value="25"/>
+                        </div>
                     </div>
+                </Collapse>
             </div>
         )
     }
