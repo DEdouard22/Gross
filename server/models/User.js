e96128a3b7fb84878fc469894078219cf39384cd
Gabriel Edouard <edgabriel92@gmail.com>
2018-08-18 11:20:17 -0400

    saveGoal added to database

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
+    saveGoal: DataTypes.INTERGER,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     googleId: DataTypes.STRING,
