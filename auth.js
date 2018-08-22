0416213142ac707b521460dc481bd1223ade8fe1
Kamilah Amir-Al-Sharif <kamilahamir-al-sharif@Kamilahs-MacBook-Pro-2.local>
2018-08-22 00:16:58 -0400

    auth3

--- auth.js
+++ auth.js
@@ -20,7 +20,7 @@ const setupAuth = (app) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.APP_URL}/auth/google`
+        callbackURL: `${process.env.APP_URL}/auth/google/callback`
     },
     function(accessToken, refreshToken, profile, done) {
         models.User.findOrCreate({
