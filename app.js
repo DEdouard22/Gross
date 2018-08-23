531c824fa810b743dfbb6d7e49f712411dc1d55a
Kamilah Amir-Al-Sharif <kamilahamir-al-sharif@Kamilahs-MacBook-Pro-2.local>
2018-08-22 22:47:52 -0400

    routes6

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ console.log("after")
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/App.js'));
 });
 
 // catch 404 and forward to error handler
